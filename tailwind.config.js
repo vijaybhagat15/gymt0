da45822a4a7c0ad205bdcf4ba43d608b3b9ed300
vijaybhagat15 <vijaybhagatmitlab@gmail.com>
2025-01-02 11:02:28 +0530

    updatin tel.config

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,8 +3,19 @@ module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],  theme: {
-    extend: {},
-  },
+    extend: {
+      fontFamily: {
+        baloo: ['"Baloo Bhai"', 'cursive'], // Add 'Baloo Bhai' as a custom font
+        zenDots: ['"Zen Dots"', 'cursive'],
+      },
+      colors: {
+        'navy': '#0d3b66',
+        'custom-beige': '#faf0ca',
+        'sunshine': '#f4d35e',
+        'sunset-orange': '#ee964b',
+         'custom-orange': '#f95738',
+      },
+    },  },
   plugins: [],
 }
 
